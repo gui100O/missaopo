a9e226d0afa2dd15f3e95ea2d16698e29f2bd33b
gui100O <guilhermefasousa@gmail.com>
2025-06-02 20:23:27 -0300

    Missão POO com herança

--- produto/main.py
+++ produto/main.py
@@ -1,4 +1,9 @@
-from produto.produto import Produto as pop
+from produto import Produto as pop
+from produto import Produtonac as pronac
+from produto import ProdutoImport as proimp
 
 p = pop("Teclado", 100.0, 20)
 p.exibirdetails()
+p = proimp("Celular", 2000.0, 5, 0.15)
+p.exibirdetails()
+
--- produto/produto.py
+++ produto/produto.py
@@ -1,7 +1,21 @@
-class Produto():
+class Produto:
     def __init__(self, nome, preco, estoque):
         self.nome = nome
         self.preco = preco
         self.estoque = estoque
+
     def exibirdetails(self):
-        print(f"Produto: {self.nome} Preço: {self.preco} Estoque: {self.estoque} unidades.")
+        print(f"Produto: {self.nome} | Preço: R${self.preco:.2f} | Estoque: {self.estoque} unidades.")
+
+class Produtonac(Produto):
+    def __init__(self, nome, preco, estoque):
+        super().__init__(nome, preco, estoque)
+
+class ProdutoImport(Produto):
+    def __init__(self, nome, preco, estoque, taxa_de_import):
+        super().__init__(nome, preco, estoque)
+        self.taxa_de_import = taxa_de_import
+
+    def exibirdetails(self):
+        super().exibirdetails()
+        print(f"Taxa de Importação: {self.taxa_de_import * 100:.1f}%")
